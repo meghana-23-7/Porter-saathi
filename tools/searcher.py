a5d1398ef5b57ac0b64398f38afec1a0ce919083
meghana-23-7 <83748460+meghana-23-7@users.noreply.github.com>
2025-09-13 17:41:37 +0530

    Update searcher.py

--- tools/searcher.py
+++ tools/searcher.py
@@ -1,16 +1,22 @@
 from openai import OpenAI
-def ask_gemini(prompt: str):
+
+def ask_gpt(user_prompt: str, system_prompt: str) -> str:
+    print("User promt: ", user_prompt)
+    print("System prompt: ", system_prompt)
     client = OpenAI(
-        api_key="key", 
-        base_url="https://openrouter.ai/api/v1" 
+        api_key="key",  
+        base_url="https://openrouter.ai/api/v1"
     )
 
     response = client.chat.completions.create(
-        model="google/gemini-flash-1.5", 
+        model="google/gemini-flash-1.5",
         messages=[
-            {"role": "system", "content": "You are Porter Saathi, an empathetic voice-first assistant."},
-            {"role": "user", "content": prompt}
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}
         ]
     )
-    print(response)
-    print(response.choices[0].message.content)
+    try:
+        return response.choices[0].message.content.strip()
+    except Exception as e:
+        print("Error:", e)
+        return "Sorry, I couldn't process your request at the moment."
